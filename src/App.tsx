24055d3acd5f609e516e2d1685a13bdc7ec02aa6
Avinash Thakur <blaze.avinash@gmail.com>
2021-06-23 06:13:42 +0530

    changes for github page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { I18nProvider } from './core/i18n/i18nProvider';
 const App = () => {
  
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/build">
       <AppThemeProvider>
         <AuthInit>
           <I18nProvider>
