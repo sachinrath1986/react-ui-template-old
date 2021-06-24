65c44fc823193d406e65938a94449342f5e64908
lokesh-sadala <85744020+lokesh-sadala@users.noreply.github.com>
2021-06-24 17:41:43 +0530

    Ionic Integeration Changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic.app',
+  appName: 'react-ui-template',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
